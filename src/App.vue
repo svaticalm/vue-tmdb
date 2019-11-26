2fde9b52115e7d659ab6022199ebf44c686dc71c
svaticalm <svaticalm@gmail.com>
2019-11-26 17:00:38 +0500

    Добавил модальное окно с детальной информацией

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
+
 <template>
   <div id="app">
+    <div class="film-detail-modal" id="film-detail-modal" :class="{'modal-open': modalBool}" >
+      <div class="back-black" @click="modalBool =false;">
+
+      </div>
+      <div class="modal-inner">
+        <div class="modal-close" @click="modalBool = false; videosrc = ''">
+          <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          viewBox="0 0 47.971 47.971" style="enable-background:new 0 0 47.971 47.971;" xml:space="preserve">
+            <g>
+              <path d="M28.228,23.986L47.092,5.122c1.172-1.171,1.172-3.071,0-4.242c-1.172-1.172-3.07-1.172-4.242,0L23.986,19.744L5.121,0.88
+              c-1.172-1.172-3.07-1.172-4.242,0c-1.172,1.171-1.172,3.071,0,4.242l18.865,18.864L0.879,42.85c-1.172,1.171-1.172,3.071,0,4.242
+              C1.465,47.677,2.233,47.97,3,47.97s1.535-0.293,2.121-0.879l18.865-18.864L42.85,47.091c0.586,0.586,1.354,0.879,2.121,0.879
+              s1.535-0.293,2.121-0.879c1.172-1.171,1.172-3.071,0-4.242L28.228,23.986z"/>
+            </g>
+          </svg>
+        </div>
+        <div class="overview-block">
+          <img :src="'https://image.tmdb.org/t/p/w500/' + filmdetail.poster_path" alt="">
+          <div class="info">
+            <p :id="'id'+index" class="title"><b>{{filmdetail.title}}</b> <br> {{filmdetail.original_title}}</p>
+            <p class="desc">{{filmdetail.overview}}</p>
+          </div>
+        </div>
+        <iframe v-if="videosrc != ''" width="100%" height="315" :src="videosrc" frameborder="0" allowfullscreen></iframe>
+        <div v-if="videosrc == ''">
+          Трейлер не найден!
+        </div>
+      </div>
+    </div>
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>Фильмы The Movie Database</h1>
     <ul class="cats">
@@ -11,7 +41,7 @@
         <div class="info">
           <p :id="'id'+index" class="title"><b>{{result.title}}</b> <br> {{result.original_title}}</p>
           <p class="desc">{{result.overview.trimtxt(200)}}</p>
-          <div class="butt-detail">
+          <div class="butt-detail" v-on:click="getFilm(result.id);">
             Подробнее
           </div>
         </div>
@@ -31,15 +61,14 @@ export default {
       results: [],
       errors: [],
       currentIndex: '',
+      filmdetail: {videos: ''},
+      videosrc: '',
+      modalBool: false,
       cats: [{id: 1,name: 'Популярные',catname:'popular'},{id: 2,name:'Лучшие',catname:'top_rated'},{id: 3,name:'Ожидаемые',catname: 'upcoming'},{id: 4, name: 'Смотрят сейчас',catname: 'now_playing'}],
     }
   },
   mounted(){
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=7e00b848ffbb0a2bb957f6631e1ad255&language=ru-RU')
-     .then(response => {this.results = response.data == 'error' ? [] : response.data.results;})
-     .catch(error => {
-           this.errors.push(error);
-     });
+    this.fetchData('popular');
   },
   methods: {
     fetchData: function(catname){
@@ -50,11 +79,112 @@ export default {
              this.errors.push(error);
        });
     },
+    getFilm: function(id){
+      axios.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=7e00b848ffbb0a2bb957f6631e1ad255&language=ru-RU')
+       .then(response => {this.filmdetail = response.data == 'error' ? [] : response.data;})
+       .catch(error => {
+             this.errors.push(error);
+       });
+       axios.get('https://api.themoviedb.org/3/movie/' + id + '/videos?api_key=7e00b848ffbb0a2bb957f6631e1ad255&language=ru-RU')
+        .then(response => {this.filmdetail.videos = response.data == 'error' ? [] : response.data.results; this.videosrc = this.filmdetail.videos.length > 0 ? 'https://www.youtube.com/embed/' + this.filmdetail.videos[0].key : ''})
+        .catch(error => {
+              this.errors.push(error);
+        });
+        let self = this;
+        setTimeout(function(){
+          self.modalBool = true;
+        },300);
+    },
   },
 }
 </script>
 
 <style>
+.overview-block{
+  display: flex;
+  margin-bottom: 40px;
+}
+.overview-block img{
+  width: 200px;
+  margin-right: 20px;
+}
+.overview-block .info{
+  text-align: left;
+}
+.overview-block .info .title{
+  font-size: 16px;
+  color: #6e6e6e;
+  line-height: 25px;
+}
+.overview-block .info .title b{
+  color: #000;
+  font-size: 18px;
+}
+.overview-block .info .desc{
+  color: #6e6e6e;
+}
+.back-black{
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  background-color: rgba(0,0,0,0.3);
+}
+.film-detail-modal{
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  z-index: 1000;
+  position: fixed;
+  display: none;
+  justify-content: center;
+  align-items: center;
+}
+.modal-open{
+  display: flex;
+}
+.modal-inner{
+  width: 600px;
+  height: auto;
+  border-radius: 5px;
+  padding: 20px;
+  background-color: #fff;
+  position: relative;
+  animation: modalopen 0.5s ease;
+}
+.modal-close{
+  width: 15px;
+  cursor: pointer;
+  position: absolute;
+  z-index: 10;
+  right: 20px;
+  top: 20px;
+  height: 15px;
+}
+@keyframes listupdate {
+  0%{
+    opacity: 0;
+    transform: scale(0.9);
+  }
+  100%{
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+@keyframes modalopen{
+  0%{
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  100%{
+    opacity: 1;
+    transform: scale(1);
+  }
+}
 .cn-active{
   color: #000 !important;
 }
@@ -118,6 +248,7 @@ h1{
   cursor: pointer;
   margin-bottom: 20px;
   transition: all .2s;
+  animation: listupdate 0.5s ease;
 }
 .list-item:hover{
   box-shadow: 0 0 20px 3px rgba(0,0,0,0.25);
